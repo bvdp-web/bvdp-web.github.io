b3be149462c449b2cc6a1fa18e718879a5d90b00
Ignotum77 <258382800+Ignotum77@users.noreply.github.com>
2026-02-19 14:41:25 +0100

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// Dark mode toggle
+const toggle = document.getElementById("darkToggle");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+toggle?.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
---
+++ posts/first-post.md
@@ -0,0 +1,12 @@
+# My First Post
+
+This is written in **Markdown**.
+
+- Easy to write
+- Clean
+- Developer friendly
+
+## Code Example
+
+```js
+console.log("Hello world");
