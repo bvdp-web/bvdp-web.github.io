513a604275648063c0250f0acab579c955424a52
Ignotum77 <258382800+Ignotum77@users.noreply.github.com>
2026-02-19 16:57:18 +0100

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -14,3 +14,22 @@ toggle?.addEventListener("click", () => {
     localStorage.setItem("theme", "light");
   }
 });
+
+
+// Youtube embeds
+document.querySelectorAll(".youtube .card").forEach(card => {
+  card.addEventListener("click", () => {
+    const videoUrl = card.dataset.video;
+    if (!card.querySelector("iframe")) {
+      card.innerHTML = `
+        <iframe 
+          src="${videoUrl}&autoplay=1" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          allowfullscreen
+          style="width:100%; height:100%; aspect-ratio:16/9; border-radius:10px;"
+        ></iframe>
+      `;
+    }
+  });
+});
