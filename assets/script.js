4cc77ac42e5ddac30e271e85be8e233f6f2dc4eb
Ignotum77 <258382800+Ignotum77@users.noreply.github.com>
2026-02-20 12:05:57 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -22,21 +22,17 @@ document.getElementById("hamburger")
 
 // Dark mode toggle
 const toggle = document.getElementById("darkToggle");
-
 if (localStorage.getItem("theme") === "dark") {
   document.body.classList.add("dark");
 }
-
 toggle?.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-
   if (document.body.classList.contains("dark")) {
     localStorage.setItem("theme", "dark");
   } else {
     localStorage.setItem("theme", "light");
   }
 });
-
 
 // Youtube embeds
 document.querySelectorAll(".youtube .thumbnail").forEach(thumb => {
