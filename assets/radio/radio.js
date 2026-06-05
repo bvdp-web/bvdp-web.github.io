8a89211a2ba9f989e235eb83497d645f753dd178
Ignotum77 <258382800+Ignotum77@users.noreply.github.com>
2026-06-05 23:34:52 +0200

    Update radio.js

--- assets/radio/radio.js
+++ assets/radio/radio.js
@@ -94,6 +94,14 @@ function scheduleReconnect() {
   if (reconnectAttempts >= MAX_RETRIES) {
     player.pause();
     console.warn("Stream temporarily unavailable, giving up");
+    if (!reconnectResetTimer) {
+      reconnectResetTimer = setTimeout(() => {
+        console.log("Reconnect attempts reset after 60s");
+        reconnectAttempts = 0;
+        reconnectResetTimer = null;
+        scheduleReconnect(); // try reconnecting again
+      }, 60000);
+    }
     return;
   }
   reconnectTimer = setTimeout(async () => {
