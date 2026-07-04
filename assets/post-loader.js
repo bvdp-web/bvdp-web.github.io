73250475d4d82e718ee3731c00d96a4f2c60f983
Ignotum77 <258382800+Ignotum77@users.noreply.github.com>
2026-07-04 21:08:41 +0200

    Update post-loader.js

--- assets/post-loader.js
+++ assets/post-loader.js
@@ -65,7 +65,8 @@
     textNodes.forEach(textNode => {
       const parent = textNode.parentNode;
       const text = textNode.nodeValue;
-      text = text.replace(/([\u0590-\u05FF\uFB1D-\uFB4F״׳־׃]+)\.(\d+)/g, "$2.$1");
+      text = text.split(" / ").reverse().join(" / "); // normalize ordering FIRST
+      text = text.replace(/([\u0590-\u05FF\uFB1D-\uFB4F״׳־׃]+)\.(\d+)/g, "$2.$1");  // fix lemma numbering order
       const fragment = document.createDocumentFragment();
       let lastIndex = 0;
       const combinedRegex = /([\u0590-\u05FF\uFB1D-\uFB4F״׳־׃]+(?:\s+[\u0590-\u05FF\uFB1D-\uFB4F״׳־׃]+)*)|([\u0370-\u03FF\u1F00-\u1FFF]+)/g;
