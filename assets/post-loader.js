8e740b69f9043ae2f9206324bc327e787ddadb77
Ignotum77 <258382800+Ignotum77@users.noreply.github.com>
2026-07-05 11:18:34 +0200

    Update post-loader.js

--- assets/post-loader.js
+++ assets/post-loader.js
@@ -113,7 +113,7 @@
         } else {
           // Mixed → inline processing
           el.appendChild(renderInline(lineText, isHeading, lang));
-          // el.appendChild(document.createElement("br"));
+          el.appendChild(document.createElement("br"));
         }
         lineText = "";
       }
