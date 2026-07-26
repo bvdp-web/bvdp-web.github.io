7dca8c9d93f715e4f011d4487f4dcd0f0a98d688
Ignotum77 <258382800+Ignotum77@users.noreply.github.com>
2026-07-26 19:52:27 +0200

    Update post-loader.js

--- assets/post-loader.js
+++ assets/post-loader.js
@@ -156,6 +156,15 @@
     .use(window.markdownitMark)
     .use(window.markdownitSub)
     .use(window.markdownitSup);
+  const defaultLinkOpen = 
+    md.renderer.rules.link_open ||
+    ((tokens, idx, options, env, self) =>
+      self.renderToken(tokens, idx, options));
+  md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+    tokens[idx].attrSet("target", "_blank");
+    tokens[idx].attrSet("rel", "noopener noreferrer");
+    return defaultLinkOpen(tokens, idx, options, env, self);
+  };
   // --- LoadPost Function ---
   async function loadPost() {
     if (!post) return showNotFound();
